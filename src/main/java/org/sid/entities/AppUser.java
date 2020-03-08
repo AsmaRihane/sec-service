539109344c90a4ca5a09d0e54735081cb7c5315b
AsmaRihane <rihaneassma@gmail.com>
2020-03-08 21:25:17 +0100

    first commit

---
+++ src/main/java/org/sid/entities/AppUser.java
@@ -0,0 +1,54 @@
+package org.sid.entities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.*;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class AppUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	@Column(unique= true)
+	private String username;
+	private String password;
+	private boolean activated;
+	@ManyToMany(fetch=FetchType.EAGER)
+	private Collection<AppRole> roles= new ArrayList<>();
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isActivated() {
+		return activated;
+	}
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
+	public Collection<AppRole> getRoles() {
+		return roles;
+	}
+	public void setRoles(Collection<AppRole> roles) {
+		this.roles = roles;
+	}
+	
+
+}
